38c602c4fdc676ddaf952f003ca602825bb8703c
rahulmhjn <rahul.mahajan676@gmail.com>
2020-07-18 15:35:56 +0530

    add & delete transaction

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+  var amount = transactions.map((transaction) => transaction.amount);
+  var bal = amount.reduce((acc, curr) => acc + curr, 0).toFixed(2);
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${bal}</h1>
     </>
   );
 };
