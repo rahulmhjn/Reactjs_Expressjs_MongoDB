fff8b181af3f312172b0fe86c68b1459cbc773ff
rahulmhjn <rahul.mahajan676@gmail.com>
2020-07-22 01:54:47 +0530

    build

---
+++ client/build/precache-manifest.c5a1dee7c9965ea53eb6954513b77919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b50bc7e07f78675d27432b860e7922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3dfd1d71e22ea268769",
+    "url": "/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "743cb3f389247e8c735d",
+    "url": "/static/js/2.b4006025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4006025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3dfd1d71e22ea268769",
+    "url": "/static/js/main.0ad66b2f.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
